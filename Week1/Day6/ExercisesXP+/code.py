317f5b152745dc9f4c6e9e086fafc4a130663c1a
NivYakir <niv.yakir1008@gmail.com>
2025-07-26 18:50:45 +0300

    updated dictionaries exercisesXP + and Gold

--- Week1/Day6/ExercisesXP+/code.py
+++ Week1/Day6/ExercisesXP+/code.py
@@ -1,45 +1,45 @@
 import math
 
-# # Exercise 1:
-# student_grades = {
-#     "Alice": [88, 92, 100],
-#     "Bob": [75, 78, 80],
-#     "Charlie": [92, 90, 85],
-#     "Dana": [83, 88, 92],
-#     "Eli": [78, 80, 72]
-# }
+# Exercise 1:
+student_grades = {
+    "Alice": [88, 92, 100],
+    "Bob": [75, 78, 80],
+    "Charlie": [92, 90, 85],
+    "Dana": [83, 88, 92],
+    "Eli": [78, 80, 72]
+}
 
-# # a
-# student_averages = {}
+# a
+student_averages = {}
 
-# for name, grade in student_grades.items():
-#     student_averages.update({name : (sum(grade) / len(grade))})
-# print(student_averages)
+for name, grade in student_grades.items():
+    student_averages.update({name : (sum(grade) / len(grade))})
+print(student_averages)
 
-# # b
-# student_letter_grades = {}
-# for name, grade in student_averages.items():
-#     if grade >= 90:
-#         student_letter_grades.update({name : 'A'})
-#     elif grade >= 80 and grade <= 89:
-#         student_letter_grades.update({name : 'B'})
-#     elif grade >= 70 and grade <= 79:
-#         student_letter_grades.update({name : 'C'})
-#     elif grade >= 60 and grade <= 69:
-#         student_letter_grades.update({name : 'D'})
-#     else:
-#         student_letter_grades.update({name : 'F'})
-# print(student_letter_grades)
+# b
+student_letter_grades = {}
+for name, grade in student_averages.items():
+    if grade >= 90:
+        student_letter_grades.update({name : 'A'})
+    elif grade >= 80 and grade <= 89:
+        student_letter_grades.update({name : 'B'})
+    elif grade >= 70 and grade <= 79:
+        student_letter_grades.update({name : 'C'})
+    elif grade >= 60 and grade <= 69:
+        student_letter_grades.update({name : 'D'})
+    else:
+        student_letter_grades.update({name : 'F'})
+print(student_letter_grades)
 
-# class_average = 0
-# for grade in student_averages.values():
-#     class_average += grade
-# print(class_average/len(student_averages.values()))
+class_average = 0
+for grade in student_averages.values():
+    class_average += grade
+print(class_average/len(student_averages.values()))
 
-# # Print the name of each student, their average grade, and their letter grade.
+# Print the name of each student, their average grade, and their letter grade.
 
-# for name, grades in student_grades.items():
-#     print(f"{name}'s average grade is {student_averages[name]} and letter grade is {student_letter_grades[name]}")
+for name, grades in student_grades.items():
+    print(f"{name}'s average grade is {student_averages[name]} and letter grade is {student_letter_grades[name]}")
 
 # Exercise 2: Advanced Data Manipulation and Analysis
 
@@ -53,29 +53,29 @@ sales_data = [
     {"customer_id": 1, "product": "Headphones", "price": 100, "quantity": 2, "date": "2023-04-09"},
 ]
 
-# # 1
-# total_sales = {}
-# for entry in sales_data:
-#     product = entry["product"]
-#     cost = entry["price"] * entry["quantity"]
-#     if product in total_sales:
-#         total_sales[product] += cost
-#     else:
-#         total_sales[product] = cost
+# 1
+total_sales = {}
+for entry in sales_data:
+    product = entry["product"]
+    cost = entry["price"] * entry["quantity"]
+    if product in total_sales:
+        total_sales[product] += cost
+    else:
+        total_sales[product] = cost
 
-# print(total_sales)
+print(total_sales)
 
-# # 2
-# customer_spend = {}
-# for entry in sales_data:
-#     customer = entry["customer_id"]
-#     cost = entry["price"] * entry["quantity"]
-#     if customer in customer_spend:
-#         customer_spend[customer] += cost
-#     else:
-#         customer_spend[customer] = cost
+# 2
+customer_spend = {}
+for entry in sales_data:
+    customer = entry["customer_id"]
+    cost = entry["price"] * entry["quantity"]
+    if customer in customer_spend:
+        customer_spend[customer] += cost
+    else:
+        customer_spend[customer] = cost
 
-# print(customer_spend)
+print(customer_spend)
 
 # 3
 for transaction in sales_data:
