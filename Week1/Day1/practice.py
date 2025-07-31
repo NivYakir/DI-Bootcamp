0bb30b97f1da410418652495f390e84099bbc5e1
NivYakir <niv.yakir1008@gmail.com>
2025-07-31 12:36:03 +0300

    Final Version of Hangman Uploaded

--- Week1/Day1/practice.py
+++ Week1/Day1/practice.py
@@ -1,128 +1,237 @@
-# import random
+# # import random
 
-# # # Timed Challenge: Functions
+# # # # Timed Challenge: Functions
 
-# # def count_occurence():
-# #     my_string = input("Enter a string:\n")
-# #     check = input("Enter a letter:\n")
-# #     counter = 0
-# #     for char in my_string:
-# #         if char == check:
-# #             counter += 1
-# #     return counter
+# # # def count_occurence():
+# # #     my_string = input("Enter a string:\n")
+# # #     check = input("Enter a letter:\n")
+# # #     counter = 0
+# # #     for char in my_string:
+# # #         if char == check:
+# # #             counter += 1
+# # #     return counter
 
-# # print(count_occurence())
+# # # print(count_occurence())
 
-# # my_string = 'Tommorow yesterday was today'
-# # print(list(my_string))
+# # # my_string = 'Tommorow yesterday was today'
+# # # print(list(my_string))
 
-# # def count_occurence_2():
-# #     my_string = input("Enter a string:\n")
-# #     check = input("Enter a letter:\n")
-# #     temp = list(my_string)
-# #     return temp.count(check)
+# # # def count_occurence_2():
+# # #     my_string = input("Enter a string:\n")
+# # #     check = input("Enter a letter:\n")
+# # #     temp = list(my_string)
+# # #     return temp.count(check)
 
-# # print(count_occurence())
+# # # print(count_occurence())
 
-# # # Exercise 1: Sum
+# # # # Exercise 1: Sum
 
-# # def get_sum(x):
+# # # def get_sum(x):
+# # #     result = []
+# # #     for num in range(1,5):
+# # #         my_str = ""
+# # #         my_str += (num * x)
+# # #         my_num = int(my_str)
+# # #         result.append(my_num)
+# # #     return sum(result)
+
+# # # print(get_sum('3'))
+
+# # # # Exercise 2: Double Dice
+
+# # # def throw_dice():
+# # #     '''Returns a number between 1 and 6'''
+# # #     return random.randint(1,6)
+
+# # # def throw_until_doubles():
+# # #     counter = 0
+# # #     while True:
+# # #         roll_1 = throw_dice()
+# # #         roll_2 = throw_dice()
+# # #         counter += 1
+# # #         if roll_1 == roll_2:
+# # #             break
+# # #     return counter
+
+# # # def main():
+# # #     result = []
+# # #     for _ in range(100):
+# # #         result.append(throw_until_doubles())
+# # #     print(result)
+# # #     return sum(result)
+
+# # # print(main())
+
+# # # my_string = input(f"Enter a string:\n")
+# # # result = {}
+# # # for i, char in enumerate(my_string):
+# # #     if char not in result.keys():
+# # #         result.update({char : [i]})
+# # #     else:
+# # #         result[char].append(i)
+
+# # # print(result)
+
+
+# # MATRIX_STR = '''7ii
+# # Tsx
+# # h%?
+# # i #
+# # sM 
+# # $a 
+# # #t%'''
+
+
+# # def string_to_matrix(text):
+# #     '''Converts a string to a 2D Matrix'''
 # #     result = []
-# #     for num in range(1,5):
-# #         my_str = ""
-# #         my_str += (num * x)
-# #         my_num = int(my_str)
-# #         result.append(my_num)
-# #     return sum(result)
+# #     temp = text.split('\n')
+# #     for row in temp:
+# #         rows = []
+# #         for char in row:
+# #             rows.append(char)
+# #         result.append(rows)
+# #     return result
 
-# # print(get_sum('3'))
+# # print(string_to_matrix(MATRIX_STR))
 
-# # # Exercise 2: Double Dice
+# # def matrix_decode():
+# #     result = ''
+# #     matrix = string_to_matrix(MATRIX_STR)
+# #     rows = len(matrix)
+# #     columns = len(matrix[0])
 
-# # def throw_dice():
-# #     '''Returns a number between 1 and 6'''
-# #     return random.randint(1,6)
-
-# # def throw_until_doubles():
-# #     counter = 0
-# #     while True:
-# #         roll_1 = throw_dice()
-# #         roll_2 = throw_dice()
-# #         counter += 1
-# #         if roll_1 == roll_2:
-# #             break
-# #     return counter
-
-# # def main():
-# #     result = []
-# #     for _ in range(100):
-# #         result.append(throw_until_doubles())
+# #     for c in range(columns):
+# #         for r in range(rows):
+# #             letter = matrix[r][c]
+# #             if letter.isalpha() == True:
+# #                 result += letter
 # #     print(result)
-# #     return sum(result)
 
-# # print(main())
+# # matrix_decode()
 
-# # my_string = input(f"Enter a string:\n")
-# # result = {}
-# # for i, char in enumerate(my_string):
-# #     if char not in result.keys():
-# #         result.update({char : [i]})
-# #     else:
-# #         result[char].append(i)
+# # board = [[" " for cells in range(3)] for cells in range(3)]
 
-# # print(result)
-
-
-# MATRIX_STR = '''7ii
-# Tsx
-# h%?
-# i #
-# sM 
-# $a 
-# #t%'''
-
-
-# def string_to_matrix(text):
-#     '''Converts a string to a 2D Matrix'''
-#     result = []
-#     temp = text.split('\n')
-#     for row in temp:
-#         rows = []
-#         for char in row:
-#             rows.append(char)
-#         result.append(rows)
-#     return result
-
-# print(string_to_matrix(MATRIX_STR))
-
-# def matrix_decode():
-#     result = ''
-#     matrix = string_to_matrix(MATRIX_STR)
-#     rows = len(matrix)
-#     columns = len(matrix[0])
-
-#     for c in range(columns):
-#         for r in range(rows):
-#             letter = matrix[r][c]
-#             if letter.isalpha() == True:
-#                 result += letter
-#     print(result)
-
-# matrix_decode()
-
-# board = [[" " for cells in range(3)] for cells in range(3)]
-
-# print(board)
+# # print(board)
 
 
 
 
 
-my_word = 'python'
-my_board = ['*' for char in my_word]
-user_input = 't'
+# # my_word = 'pytthon'
+# # my_board = ['*' for char in my_word]
+# # user_input = 't'
 
-for i in range(len(my_board)):
-    if user_input == my_word[i]:
-        my_board[i] = user_input
-print(my_board)
+# # for i in range(len(my_board)):
+# #     if user_input == my_word[i]:
+# #         my_board[i] = user_input
+# # print(my_board)
+
+# word = 'pythagarusursu'
+# board = ['g' for char in word]
+# guessed_letters = []
+
+# def display_gallows(attempt):
+#     '''Displays the hangman's status'''
+#     board_stage = ["""
+#             -------
+#             |  O  |
+#             | /|\\ |
+#             | / \\ |
+#             -------
+#     """,
+#         """
+#             -------
+#             |  O  |
+#             | /|\\ |
+#             | /   |
+#             -------
+#     """,
+#     """
+#             -------
+#             |  O  |
+#             | /|\\ |
+#             |     |
+#             -------
+#     """,
+#     """
+#             -------
+#             |  O  |
+#             | /|  |
+#             |     |
+#             -------
+#     """,
+#     """
+#             -------
+#             |  O  |
+#             |  |  |
+#             |     |
+#             -------
+#     """,
+#     """
+#             -------
+#             |  O  |
+#             |     |
+#             |     |
+#             -------
+#     """,
+#     """
+#             -------
+#             |     |
+#             |     |
+#             |     |
+#             -------
+#     """]
+
+#     print(board_stage[attempt])
+
+# def letter_check(board,word,guessed_letters):
+#     attempt = 6
+#     while True:
+#         user_input = input("Guess a letter: ").lower()
+#         print(user_input)
+#         if len(user_input) == 1 and (user_input.isalpha() == True):
+#             if user_input in guessed_letters:
+#                 print("You have already guessed this letter")
+#             else: 
+#                 if user_input in word:
+#                     for i in range(len(board)):
+#                         if user_input == word[i]:
+#                             board[i] = user_input
+#                             guessed_letters.append(user_input)
+#                     break
+#                 else:
+#                     guessed_letters.append(user_input)
+#                     attempt -= 1
+#                     display_gallows(attempt)
+#                     break
+#         else:
+#             print("Invalid input.")
+#     print(board)
+
+# def win_check(board):
+#     '''Returns True if all the letters have been guessed correctly'''
+#     if '*' in board:
+#         return False
+#     else:
+#         return True
+
+
+# print(win_check(board))
+
+player = 2
+
+def true_or_false():
+    if player == 1:
+        return True
+    else:
+        return False
+    
+
+attempt = 0
+while attempt != 6:
+    if true_or_false == True:
+        print('Okay')
+    else:
+        print('Not okay')
+        attempt += 1
--- Week2/Day3/HangMan/cody.py
+++ Week2/Day3/HangMan/cody.py
@@ -8,17 +8,31 @@ def random_word():
 def create_board(word):
     return ['*' for char in word]
 
-def letter_check(word, board):
+def display_board(board):
+    print(f"Board Status: {board}")
+
+def letter_check(board,word,guessed_letters):
     while True:
         user_input = input("Guess a letter: ").lower()
-        if len(user_input) != 1 or user_input.isalpha() == False:
-            for i in range(len(board)):
-                if user_input == word[i]:
-                    board[i] = user_input
-                    break
+        print(user_input)
+        if len(user_input) == 1 and (user_input.isalpha() == True):
+            if user_input in guessed_letters:
+                print("You have already guessed this letter")
+            else: 
+                if user_input in word:
+                    for i in range(len(board)):
+                        if user_input == word[i]:
+                            board[i] = user_input
+                            if user_input not in guessed_letters:
+                                guessed_letters.append(user_input)
+                    return True
+                else:
+                    if user_input not in guessed_letters:
+                        guessed_letters.append(user_input)
+                        print("Letter is not in the word.")
+                    return False
         else:
             print("Invalid input.")
-            continue
 
 
 def display_gallows(attempt):
@@ -52,11 +66,11 @@ def display_gallows(attempt):
             -------
     """,
     """
-            -----
-            | O |
-            | | |
-            |   |
-            -----
+            -------
+            |  O  |
+            |  |  |
+            |     |
+            -------
     """,
     """
             -------
@@ -72,3 +86,44 @@ def display_gallows(attempt):
             |     |
             -------
     """]
+
+    print(board_stage[attempt])
+
+
+def win_check(board):
+    '''Returns True if all the letters have been guessed correctly'''
+    if '*' in board:
+        return False
+    else:
+        return True
+
+
+def main():
+    word = random_word()
+    board = create_board(word)
+    guessed_letters = []
+    attempt = 6
+    print("Welcome to Hangman! Try to guess the word by inputting a letter. You have 6 tries to guess all the letters!")
+    while attempt > 0:
+        print(f"You have {attempt} tries left!")
+        display_board(board)
+        print(f"Guessed Letters: {guessed_letters}")
+        display_gallows(attempt)
+        if letter_check(board,word,guessed_letters) == False:
+            attempt -= 1
+        if win_check(board) == True:
+            break
+    if win_check(board) == False:
+        print("You lose!")
+    else:
+        print("You win!")
+    display_gallows(attempt)
+    print(f"The word was {word}")
+
+
+main()
+
+
+
+
+
