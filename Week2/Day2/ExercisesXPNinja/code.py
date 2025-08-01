beda37c7133eb004ca76eb2d00ac23dd554ea466
NivYakir <niv.yakir1008@gmail.com>
2025-08-01 13:33:54 +0300

    updates to dailychallenges and exercises

--- Week2/Day2/ExercisesXPNinja/code.py
+++ Week2/Day2/ExercisesXPNinja/code.py
@@ -1,57 +1,57 @@
-# # Exercise 1: What's Your Name?
+# Exercise 1: What's Your Name?
 
-# def get_full_name(first_name = 'niv', last_name = 'yakir', middle_name=''):
-#     first_name, last_name, middle_name = first_name.capitalize(), last_name.capitalize(), middle_name.capitalize()
-#     if middle_name == '':
-#         return f"{first_name} {last_name}"
-#     else:
-#         return f"{first_name} {middle_name} {last_name}"
+def get_full_name(first_name = 'niv', last_name = 'yakir', middle_name=''):
+    first_name, last_name, middle_name = first_name.capitalize(), last_name.capitalize(), middle_name.capitalize()
+    if middle_name == '':
+        return f"{first_name} {last_name}"
+    else:
+        return f"{first_name} {middle_name} {last_name}"
 
-# print(get_full_name())
+print(get_full_name())
 
-# # Exercise 2: From English to Morse
+# Exercise 2: From English to Morse
 
-# morse_code_dict = {
-#     'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',
-#     'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---',
-#     'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',
-#     'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
-#     'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--',
-#     'Z': '--..',
-#     '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
-#     '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-#     '.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--', ':': '---...',
-#     "'": '.----.', '-': '-....-', '/': '-..-.', '(': '-.--.', ')': '-.--.-',
-#     '"': '.-..-.', '@': '.--.-.', '=': '-...-', ' ' : '/'
-# }
+morse_code_dict = {
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',
+    'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---',
+    'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',
+    'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+    'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--',
+    'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--', ':': '---...',
+    "'": '.----.', '-': '-....-', '/': '-..-.', '(': '-.--.', ')': '-.--.-',
+    '"': '.-..-.', '@': '.--.-.', '=': '-...-', ' ' : '/'
+}
 
-# reversed_code_dict = {v:k for k, v in morse_code_dict.items()}
+reversed_code_dict = {v:k for k, v in morse_code_dict.items()}
 
-# def english_to_morse():
-#     '''Returns a morse encoded text'''
-#     text = input(f"Enter the text you wish to translate:\n")
-#     text = text.upper()
-#     tranlated = ""
-#     for char in text:
-#         if char == ' ':
-#             tranlated += '/ '
-#         else:
-#             tranlated += morse_code_dict[char] + " "
-#     return tranlated
+def english_to_morse():
+    '''Returns a morse encoded text'''
+    text = input(f"Enter the text you wish to translate:\n")
+    text = text.upper()
+    tranlated = ""
+    for char in text:
+        if char == ' ':
+            tranlated += '/ '
+        else:
+            tranlated += morse_code_dict[char] + " "
+    return tranlated
 
-# print(english_to_morse())
+print(english_to_morse())
 
-# def morse_to_english():
-#     '''Translates a text in morse code to English'''
-#     text = input(f"Enter the encoded text you wish to translate:\n")
-#     words = text.split(' ')
-#     cleaned = [word for word in words if word != '']
-#     tranlated = ""
-#     for word in cleaned:
-#         tranlated += reversed_code_dict[word]
-#     return tranlated
+def morse_to_english():
+    '''Translates a text in morse code to English'''
+    text = input(f"Enter the encoded text you wish to translate:\n")
+    words = text.split(' ')
+    cleaned = [word for word in words if word != '']
+    tranlated = ""
+    for word in cleaned:
+        tranlated += reversed_code_dict[word]
+    return tranlated
 
-# print(morse_to_english())
+print(morse_to_english())
 
 def insertion_sort(alist):
    '''Sorts a list of numbers from smallest to larges'''
