16ee7418e57aea210a6672905c0d2982887014a4
NivYakir <niv.yakir1008@gmail.com>
2025-08-02 20:45:18 +0300

    minor fixes and changes to OOP Exercises

--- Week2/Day6/ExercisesXP/code.py
+++ Week2/Day6/ExercisesXP/code.py
@@ -5,9 +5,10 @@ class Cat:
         self.age = cat_age
 
 # Step 1: Create cat objects
+
 cat1 = Cat('Garfield', 12)
-cat2 = Cat('Puss', 20)
-cat3 = Cat('Sylvester', 19)
+cat2 = Cat('Puss', 7)
+cat3 = Cat('Sylvester', 10)
 
 def get_oldest(cat1,cat2,cat3):
     oldest_cat = None
@@ -18,12 +19,13 @@ def get_oldest(cat1,cat2,cat3):
     else:
         oldest_cat = cat3
 
-    print(f"The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.")
+    print(f"The oldest cat is {oldest_cat.name}, and (s)he is {oldest_cat.age} years old.")
 
 get_oldest(cat1,cat2,cat3)
 
 # Exercise 2: Dogs
 # Step 1 - Create Dog Class + Methods
+
 class Dog:
     def __init__(self, name, height):
         self.name = name
@@ -36,24 +38,35 @@ class Dog:
         print(f"{self.name} jumps {self.height * 2}cm high!")
 
 # Step 2 - Create Dog objects
+
 davids_dog = Dog('Max', 64)
 sarahs_dog = Dog('Macy', 42)
 
 # Step 3 - Print Dog Details and Call Methods
-print(f"David's dog is named {davids_dog.name} and he is {davids_dog.height}cm tall.")
-print(f"Sarah's dog is named {sarahs_dog.name} and she is {sarahs_dog.height}cm tall.")
+
+print(f"David's dog is named {davids_dog.name} and his height is {davids_dog.height}cm.")
+print(f"Sarah's dog is named {sarahs_dog.name} and her height is {sarahs_dog.height}cm.")
 davids_dog.bark()
 davids_dog.jump()
 sarahs_dog.bark()
 sarahs_dog.jump()
 
 # Step 4 - Compare the Dog Sizes:
+
+taller_dog = None
+shorter_dog = None
 if davids_dog.height > sarahs_dog.height:
-    print(f"{davids_dog.name} is taller than {sarahs_dog.name} by {davids_dog.height - sarahs_dog.height}cm.")
+    taller_dog = davids_dog
+    shorter_dog = sarahs_dog
 else:
-    print(f"{sarahs_dog.name} is taller than {davids_dog.name} by {sarahs_dog.height - davids_dog.height}cm.")
+    taller_dog = sarahs_dog
+    shorter_dog = davids_dog
+
+print(f"{taller_dog.name} is taller than {shorter_dog.name} by {taller_dog.height - shorter_dog.height}cm.")
+
 
 # Exercise 3: Who's the Song Producer?
+
 class Song:
     def __init__(self,lyrics):
         self.lyrics = lyrics
@@ -62,8 +75,7 @@ class Song:
         for word in self.lyrics:
             print(word)
 
-stairway = Song(["There's a lady who's sure", "all that glitters is gold", 
-                 "and she's buying a stairway to heaven"])
+stairway = Song(["There's a lady who's sure", "all that glitters is gold", "and she's buying a stairway to heaven"])
 
 stairway.sing_me_a_song()
 
@@ -87,8 +99,10 @@ class Zoo:
     
     def sell_animal(self, animal_sold):
         '''Sell(remove) an animal if it is in the Zoo'''
-        if animal_sold in self.animals:
+        if animal_sold.upper() in self.animals.upper():
             self.animals.remove(animal_sold)
+        else:
+            print('That animal is not in the Zoo.')
         return self
 
     def sort_animals(self):
@@ -112,6 +126,7 @@ class Zoo:
 my_zoo = Zoo("Prague Zoo")
 my_zoo.add_animal('Zebra')
 my_zoo.add_animal('Gorilla')
+my_zoo.add_animal('Eagle')
 my_zoo.add_animal('Elephant')
 my_zoo.get_animals()
 my_zoo.add_animal('Giraffe')
